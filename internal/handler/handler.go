8f8d5451afffd5bf9e664e2ccf44310053941e1f
TechGG1 <dory01check+iron@gmail.com>
2023-08-08 13:34:44 +0200

    feat: change methods in InitRoutes()

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/TechGG1/Library/internal/logging"
 	"github.com/TechGG1/Library/internal/service"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type Handler struct {
@@ -20,14 +21,14 @@ func NewHandler(service *service.Service, logger logging.Logger) *Handler {
 
 func (h *Handler) InitRoutes() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/book", h.Books).Methods("GET")
-	r.HandleFunc("/book", h.CreateBook).Methods("POST")
-	r.HandleFunc("/reader", h.CreateReader).Methods("POST")
-	r.HandleFunc("/reader", h.Reader).Methods("GET")
-	r.HandleFunc("/reader", h.UpdateReader).Methods("PUT")
-	r.HandleFunc("/rent", h.CreateRent).Methods("POST")
-	r.HandleFunc("/rent", h.UpdateRent).Methods("PUT")
-	r.HandleFunc("/fine", h.Fine).Methods("GET")
-	r.HandleFunc("/rent", h.Rents).Methods("GET")
+	r.HandleFunc("/book", h.Books).Methods(http.MethodGet)
+	r.HandleFunc("/book", h.CreateBook).Methods(http.MethodPost)
+	r.HandleFunc("/reader", h.CreateReader).Methods(http.MethodPost)
+	r.HandleFunc("/reader", h.Reader).Methods(http.MethodGet)
+	r.HandleFunc("/reader", h.UpdateReader).Methods(http.MethodPut)
+	r.HandleFunc("/rent", h.CreateRent).Methods(http.MethodPost)
+	r.HandleFunc("/rent", h.UpdateRent).Methods(http.MethodPut)
+	r.HandleFunc("/fine", h.Fine).Methods(http.MethodGet)
+	r.HandleFunc("/rent", h.Rents).Methods(http.MethodGet)
 	return r
 }
