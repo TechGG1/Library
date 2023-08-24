fe4502a6a862ce473d7cd9bb32a69fa685218f61
TechGG1 <dory01check+iron@gmail.com>
2023-08-24 11:45:27 +0200

    feat: add cors middleware for communication with frontend

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -21,6 +21,9 @@ func NewHandler(service *service.Service, logger logging.Logger) *Handler {
 
 func (h *Handler) InitRoutes() *mux.Router {
 	r := mux.NewRouter()
+
+	r.Use(corsMiddleware)
+
 	r.HandleFunc("/book", h.Books).Methods(http.MethodGet)
 	r.HandleFunc("/book", h.CreateBook).Methods(http.MethodPost)
 	r.HandleFunc("/reader", h.CreateReader).Methods(http.MethodPost)
@@ -31,4 +34,19 @@ func (h *Handler) InitRoutes() *mux.Router {
 	r.HandleFunc("/fine", h.Fine).Methods(http.MethodGet)
 	r.HandleFunc("/rent", h.Rents).Methods(http.MethodGet)
 	return r
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")                                                            // 允许访问所有域，可以换成具体url，注意仅具体url才能带cookie信息
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token") //header的类型
+		w.Header().Add("Access-Control-Allow-Credentials", "true")                                                    //设置为true，允许ajax异步请求带cookie信息
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")                             //允许请求方法
+		w.Header().Set("content-type", "application/json;charset=UTF-8")                                              //返回数据格式是json
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
